70e53a0e5653c218b476104f1b689c5426ac78b7
Narendra-25008086 <narendrakrishnanks16052008@gmail.com>
2026-03-09 00:07:32 +0530

    f

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 import pytesseract
 from PIL import Image
+import os
 
 app = Flask(__name__)
 
-# Windows Tesseract path
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# Windows only path
+if os.name == "nt":
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 @app.route("/")
 def home():
