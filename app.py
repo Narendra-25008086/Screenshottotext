807d401225011e3c3a66051eed20924c4cae6152
Narendra-25008086 <narendrakrishnanks16052008@gmail.com>
2026-03-08 22:42:29 +0530

    f

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import pytesseract
+from PIL import Image
+
+app = Flask(__name__)
+
+# Windows Tesseract path
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/extract", methods=["POST"])
+def extract():
+
+    if "image" not in request.files:
+        return render_template("index.html", result="No file uploaded")
+
+    file = request.files["image"]
+
+    if file.filename == "":
+        return render_template("index.html", result="No file selected")
+
+    img = Image.open(file)
+    text = pytesseract.image_to_string(img)
+
+    return render_template("index.html", result=text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
